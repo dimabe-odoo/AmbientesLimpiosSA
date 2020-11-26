7ad251f03dccdf8ef7200d5825d0da36bf910b6e
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2020-11-26 13:04:27 -0300

    Remove communes

--- al_base/models/al_commune.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class AlCommune(models.Model):
-    _name = 'al.commune'
-    state_id = fields.Many2one('res.country.state', string='Región')
-    name = fields.Char(string='Comuna')
--- al_base/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-    al_commune_id = fields.Many2one('al.commune', string='Comuna')
-    country_id = fields.Many2one('res.country', string='País', default=lambda self: self.env[
-        'res.country'].search([('code', '=', 'CL')]))
