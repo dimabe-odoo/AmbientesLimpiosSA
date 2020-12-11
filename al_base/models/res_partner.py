3e03beebc7c813395e5e0ed669c2ecdb09b3d250
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-11 16:50:49 -0300

    test

--- al_base/models/res_partner.py
+++ al_base/models/res_partner.py
@@ -17,7 +17,8 @@ class ResPartner(models.Model):
     @api.model
     def write(self, values):
         exist = self.find_partner(values['vat'])
-        if exist:
+        models._logger.error(exist)
+        if not exist:
             raise models.ValidationError(
                 'No se puede editar ya que existe un contacto con el rut {}'.format(values['vat']))
         return super(ResPartner, self).write(values)
