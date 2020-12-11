d2f1ae2ecd95581391b020ecd536c6ca27998382
dimabe-odoo <desarrolloti@dimabe.cl>
2020-12-11 16:37:55 -0300

    test

--- al_base/models/res_partner.py
+++ al_base/models/res_partner.py
@@ -1,20 +1,25 @@
 from odoo import models, api
 from ..utils.rut_helper import RutHelper
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
     def create(self, values):
-        if self.find_partner(values['vat']):
-            raise models.ValidationError('No se puede crear el contacto ya existe uno con el rut Rut {}'.format(values['vat']))
+        if 'vat' in values.keys():
+            if self.find_partner(values['vat']):
+                raise models.ValidationError(
+                    'No se puede crear el contacto ya existe uno con el rut Rut {}'.format(values['vat']))
+
         return super(ResPartner, self).create(values)
 
     def write(self, values):
         exist = self.find_partner(values['vat'])
         if exist:
-            raise models.ValidationError('No se puede editar el contacto')
+            raise models.ValidationError(
+                'No se puede editar ya que existe un contacto con el rut {}'.format(values['vat']))
         return super(ResPartner, self).write(values)
 
     def find_partner(self, rut):
-        return self.env['res.partner'].search([('vat','=',RutHelper.format_rut(rut))])
+        return self.env['res.partner'].search([('vat', '=', RutHelper.format_rut(rut))])
