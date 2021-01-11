355be7c748d8aa5d972937401c5c7f30aa3a64e1
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-01-11 18:01:16 -0300

    Cambio de nombres de códigos

--- al_base/models/product_product.py
+++ al_base/models/product_product.py
@@ -2,5 +2,5 @@ from odoo import models, fields, api
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-    al_sku = fields.Char(string='SKU')
+    al_sku = fields.Char(string='Código Sysgestion')
     al_dun = fields.Char(string='DUN')
--- al_base/models/product_template.py
+++ al_base/models/product_template.py
@@ -2,5 +2,5 @@ from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    al_sku = fields.Char(string='SKU')
+    al_sku = fields.Char(string='Código Sysgestion')
     al_dun = fields.Char(string='DUN')
