f35c61a548cf895578e22f25be8a4bee19d19e7a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-22 12:40:49 -0300

    debug}

--- al_base/models/res_partner.py
+++ al_base/models/res_partner.py
@@ -17,8 +17,7 @@ class ResPartner(models.Model):
     def write(self, values):
         currentPartner = self.get_partner(self.id)
         existVat = self.find_partner(values['vat'])
-        raise models.ValidationError(existVat.type)
-        if existVat and not existVat.type != 'contact':
+        if existVat and existVat.type == 'contact':
             if currentPartner.vat != values['vat']:
                 raise models.ValidationError(
                     'No se puede editar ya que existe un contacto con el rut {}'.format(values['vat']))
