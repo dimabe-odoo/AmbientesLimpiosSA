e9e3300978408f690e692517eb733ad9c2516db1
dimabe-odoo <desarrolloti@dimabe.cl>
2021-02-22 12:09:04 -0300

    se corrige problema en crear y modificacion de contactos

--- al_base/models/res_partner.py
+++ al_base/models/res_partner.py
@@ -19,8 +19,9 @@ class ResPartner(models.Model):
         existVat = self.find_partner(values['vat'])
         if 'child_ids' not in values.keys():
             if len(existVat) > 1:
-                raise models.ValidationError(
-                    'No se puede editar ya que existe un contacto con el rut {}'.format(values['vat']))
+                if values['vat'] not in existVat.mapped('vat'):
+                    raise models.ValidationError(
+                        'No se puede editar ya que existe un contacto con el rut {}'.format(values['vat']))
             if existVat and not existVat.type != 'contact':
                 if currentPartner.vat != values['vat']:
                     raise models.ValidationError(
