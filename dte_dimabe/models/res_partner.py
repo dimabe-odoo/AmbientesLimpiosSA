5baabff4920b55b258763f780e09eb4d3a7a4b33
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-03-12 15:29:29 -0300

    Validacion dte email

--- dte_dimabe/models/res_partner.py
+++ dte_dimabe/models/res_partner.py
@@ -1,7 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from .helpers import emailValidator
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     mail_dte = fields.Char('Email DTE')
     turn_dte = fields.Char('Giro')
     economic_activities_dte = fields.Many2many('custom.economic.activity', string = 'Actividades Económicas')
+
+    @api.onchange('mail_dte')
+    def onChangeMailDte(self):
+        if emailValidator(self.mail_dte) == False:
+            raise models.ValidationError('El email ingresado no es válido')
