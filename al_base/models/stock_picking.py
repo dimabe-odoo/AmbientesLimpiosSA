94a2eaaa381fb75fb355e3102dc0b3e25b36e690
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-03-22 15:52:49 -0300

    bloqueo de despacho

--- al_base/models/stock_picking.py
+++ al_base/models/stock_picking.py
@@ -5,7 +5,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def button_validate(self):
-        if self.partner_id and self.partner_id.picking_warn and self.partner_id.picking_warn == 'block':
+        if (self.partner_id and self.partner_id.picking_warn and self.partner_id.picking_warn == 'block') or (self.partner_id.parent_id and self.partner_id.parent_id.picking_warn and self.partner_id.parent_id.picking_warn == 'block'):
             raise models.ValidationError(self.partner_id.picking_warn_msg)
 
         return super(StockPicking, self).button_validate()
