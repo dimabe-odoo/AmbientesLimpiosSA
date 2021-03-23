22b61f3324174183bc6d731a7345176fdde77094
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 18:14:29 -0300

    reestructure document type

--- al_base/models/account_invoice.py
+++ al_base/models/account_invoice.py
@@ -1,13 +1,14 @@
-from odoo import models
+from odoo import models, api
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def create(self, vals_list):
-        for vals in vals_list:
+    @api.model
+    def create(self, values):
+        for vals in values:
             if vals.sale_id:
                 sale_order = self.env['sale.order'].search([('id','=',vals.sale_id.id)])
                 if sale_order.l10n_latam_document_type_id:
                     vals.l10n_latam_document_type_id = sale_order.l10n_latam_document_type_id.id
 
-        return super(AccountInvoice, self).create(vals_list)
+        return super(AccountInvoice, self).create(values)
