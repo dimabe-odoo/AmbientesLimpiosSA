6d7a161412bf8a735c63aad37fb8ec7b251163a8
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 18:35:44 -0300

    reestructure document type

--- al_base/models/account_move.py
+++ al_base/models/account_move.py
@@ -5,10 +5,9 @@ class AccountMove(models.Model):
 
     @api.model
     def create(self, values):
-        for vals in values:
-            if vals == 'sale_id':
-                sale_order = self.env['sale.order'].search([('id','=',values['sale_id'])])
-                if sale_order.l10n_latam_document_type_id:
-                    vals.l10n_latam_document_type_id = sale_order.l10n_latam_document_type_id.id
+        if 'sale_id' in values.keys():
+            sale_order = self.env['sale.order'].search([('id','=',values['sale_id'])])
+            if sale_order.l10n_latam_document_type_id:
+                vals.l10n_latam_document_type_id = sale_order.l10n_latam_document_type_id.id
 
         return super(AccountMove, self).create(values)
