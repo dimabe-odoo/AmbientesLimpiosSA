477ab5c00849c1a002b708a93514c6dbf6e53dcd
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-03-23 12:33:38 -0300

    fix name

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -2,5 +2,5 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    l10n_latam_document_type_id = fields.many2one('l10n_latam.document.type')
-    
+    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type')
+
