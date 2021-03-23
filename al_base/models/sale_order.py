035fce1f430c97c0ab7721cbadce38a5176918c4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 17:58:33 -0300

    reestructure document type

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -5,11 +5,4 @@ class SaleOrder(models.Model):
     l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento")
 
 
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        if self.invoice_ids:
-            if self.invoice_ids.filtered(lambda x : x.l10n_latam_document_type_id == None):
-                self.invoice_ids[-1].write({
-                    'l10n_latam_document_type_id': self.l10n_latam_document_type_id.id
-                })
-
+   
