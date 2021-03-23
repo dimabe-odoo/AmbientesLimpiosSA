f51d82da208edc2fd09a45327a059a26ea5ea92a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 18:27:46 -0300

    reestructure document type

---
+++ al_base/models/account_move.py
@@ -0,0 +1,14 @@
+from odoo import models, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def create(self, values):
+        for vals in values:
+            if vals.sale_id:
+                sale_order = self.env['sale.order'].search([('id','=',vals.sale_id.id)])
+                if sale_order.l10n_latam_document_type_id:
+                    vals.l10n_latam_document_type_id = sale_order.l10n_latam_document_type_id.id
+
+        return super(AccountMove, self).create(values)
