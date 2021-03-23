3f28b6f9a37633bb3e54d8c8e46d3486daa6c6a9
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-23 17:44:39 -0300

    reestructure document type

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -2,4 +2,14 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type')
+    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento")
+
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        if self.invoice_ids:
+            if self.invoice_ids.filtered(lambda x : x.l10n_latam_document_type_id == None):
+                self.invoice_ids[-1].write({
+                    'l10n_latam_document_type_id': self.l10n_latam_document_type_id.id
+                })
+
