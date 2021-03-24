14128cc6a0bf09b284bdb9576bf792dcca8301a6
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 14:09:06 -0300

    test

--- al_base/models/account_move.py
+++ al_base/models/account_move.py
@@ -8,6 +8,7 @@ class AccountMove(models.Model):
         if values['invoice_origin']:
             sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
             if sale_order.l10n_latam_document_type_id:
+                values['journal_id'] = 1
                 values['l10n_latam_document_type_id'] = sale_order.l10n_latam_document_type_id.id
 
         return super(AccountMove, self).create(values)
