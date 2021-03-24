a7587b40100f26ed186bf0b74c488499278ec0e4
dimabe-odoo <desarrolloti@dimabe.cl>
2021-03-24 08:54:44 -0300

    test

--- al_base/models/account_move.py
+++ al_base/models/account_move.py
@@ -5,9 +5,8 @@ class AccountMove(models.Model):
 
     @api.model
     def create(self, values):
-        raise models.ValidationError(values.keys())
         if 'sale_line_ids' in values.keys():
-            sale_order = self.env['sale.order'].search([('id','=',values['sale_line_ids'])])
+            sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
             if sale_order.l10n_latam_document_type_id:
                 values['l10n_latam_document_type_id'] = sale_order.l10n_latam_document_type_id.id
 
