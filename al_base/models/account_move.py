3fef27b6ca2915f69892e553c1c867656a6c4ac6
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-03-31 08:31:28 -0300

    fix invoice origin

--- al_base/models/account_move.py
+++ al_base/models/account_move.py
@@ -5,10 +5,11 @@ class AccountMove(models.Model):
 
     @api.model
     def create(self, values):
-        if values['invoice_origin']:
-            sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
-            if sale_order.l10n_latam_document_type_id:
-                values['journal_id'] = 1
-                values['l10n_latam_document_type_id'] = sale_order.l10n_latam_document_type_id.id
+        if 'invoice_origin' in values.keys():
+            if values['invoice_origin']:
+                sale_order = self.env['sale.order'].search([('name','=',values['invoice_origin'])])
+                if sale_order.l10n_latam_document_type_id:
+                    values['journal_id'] = 1
+                    values['l10n_latam_document_type_id'] = sale_order.l10n_latam_document_type_id.id
 
         return super(AccountMove, self).create(values)
