11ed15e946dac2c1d3839af268d3289c794793cd
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-04-08 12:08:56 -0400

    readonly supplier lot, trazability

--- al_base/models/stock_move_line.py
+++ al_base/models/stock_move_line.py
@@ -8,7 +8,7 @@ class StockMoveLine(models.Model):
 
     @api.onchange('product_id')
     def on_change_product_id(self):
-        for item in self:
+        if self.product_id and self.product_id.tracking == "lot":
             res = {
                 'attrs': {
                     'supplier_lot': {'readonly': [('product_id.tracking', '=', 'lot')]}}
