ff7241f6942e0a02c6ea6c4deeeb9380b82e0cc9
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-08 12:29:31 -0400

    readoly supplier_lot

--- al_base/models/stock_move_line.py
+++ al_base/models/stock_move_line.py
@@ -11,7 +11,7 @@ class StockMoveLine(models.Model):
     def create(self,values):
         for value in values:
             if 'product_id' in value.keys():
-                product = self.env['product.product'].search([('id', '=', values['product_id'])])
-                values['is_loteable'] = product.tracking == 'lot'
-            return super(StockMoveLine, self).create(values)
+                product = self.env['product.product'].search([('id', '=', value['product_id'])])
+                value['is_loteable'] = product.tracking == 'lot'
+            return super(StockMoveLine, self).create(value)
 
