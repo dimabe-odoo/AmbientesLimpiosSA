a4084c920d537f281dd5e769e2acfe4c7bec0b0e
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-04-08 11:39:16 -0400

    readonly supplier lot, trazability

--- al_base/models/stock_move_line.py
+++ al_base/models/stock_move_line.py
@@ -5,3 +5,9 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     supplier_lot = fields.Char('Lote Proveedor')
+
+    is_loteable = fields.Boolean(compute='_compute_is_loteable')
+
+    def _compute_is_loteable(self):
+        return self.product_id.tracking == 'lot'
+
