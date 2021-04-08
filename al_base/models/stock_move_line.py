2881b038b26f9decb49b1d31ce7711de7d6617ca
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-08 12:16:43 -0400

    readoly supplier_lot

--- al_base/models/stock_move_line.py
+++ al_base/models/stock_move_line.py
@@ -6,11 +6,10 @@ class StockMoveLine(models.Model):
 
     supplier_lot = fields.Char('Lote Proveedor')
 
-    @api.onchange('product_id')
-    def on_change_product_id(self):
-        for item in self:
-            res = {
-                'attrs': {
-                    'supplier_lot': {'readonly': [('product_id.tracking', '=', 'lot')]}}
-            }
-            return res
+    is_loteable = fields.Boolean()
+    
+    def create(self,values):
+        if 'product_id' in values.keys():
+            product = self.env['product.product'].search([('id','=',values['product_id'])])
+            values['is_loteable'] = product.tracking == 'lot'
+        return super(StockMoveLine, self).create(values)
