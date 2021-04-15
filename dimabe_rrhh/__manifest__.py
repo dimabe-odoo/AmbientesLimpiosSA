c3527396641996ccc0e348fd3adcd206d44dd698
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-15 18:37:28 -0400

    fix

--- dimabe_rrhh/__manifest__.py
+++ dimabe_rrhh/__manifest__.py
@@ -39,7 +39,7 @@
         'views/views.xml',
         'views/templates.xml',
         'views/custom_benefits_rrhh.xml',
-        #'views/custom_data.xml',
+        'views/custom_data.xml',
         'views/custom_indicators.xml',
         'views/hr_contract.xml',
         'views/hr_salary_rule.xml',
