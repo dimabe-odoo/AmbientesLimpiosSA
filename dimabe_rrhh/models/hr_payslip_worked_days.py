88b29a6ab5ab4976c4a83f5fa75731f7de569ada
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-16 18:24:58 -0400

    claculate days

--- dimabe_rrhh/models/hr_payslip_worked_days.py
+++ dimabe_rrhh/models/hr_payslip_worked_days.py
@@ -12,8 +12,9 @@ class HrPayslipWorkedDays(models.Model):
     def _onchange_pof_days(self):
         for item in self:
             item.amount = item.payslip_id.contract_id.wage / 30 * item.number_of_days
-            week_on_month = int(30 / 7)
-            days_of_week = item.payslip_id.contract_id.resource_calendar_id.attendances_ids
-            item.number_of_hours = item.payslip_id.contract_id.resource_calendar_id.full_time_required_hours / item.payslip_id.contract_id.resource_calendar_id.hours_per_day * item.number_of_days
+            
+            days_not_working = item.payslip_id.contract_id.resource_calendar_id.full_days_on_month - item.number_of_days
+
+            item.number_of_hours = item.payslip_id.contract_id.resource_calendar_id.effective_days_on_month - days_not_working * item.payslip_id.contract_id.resource_calendar_id.hours_per_day
 
     
