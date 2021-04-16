2474290ec00db2888e781a5e0671b06ade08ffa6
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-16 16:51:32 -0400

    test

--- dimabe_rrhh/models/hr_payslip_worked_days.py
+++ dimabe_rrhh/models/hr_payslip_worked_days.py
@@ -12,5 +12,6 @@ class HrPayslipWorkedDays(models.Model):
     def _onchange_pof_days(self):
         for item in self:
             item.amount = item.payslip_id.contract_id.wage / 30 * item.number_of_days
-            item.number_of_hours = item.payslip.contract_id.resource_calendar_id.full_time_required_hours * item.number_of_days
+            item.number_of_hours = item.payslip_id.contract_id.resource_calendar_id.full_time_required_hours * item.number_of_days
 
+    
