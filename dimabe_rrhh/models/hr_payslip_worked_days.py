cfea56447c0d05798687f73c068f9a02298fe422
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-19 12:46:37 -0400

    test

--- dimabe_rrhh/models/hr_payslip_worked_days.py
+++ dimabe_rrhh/models/hr_payslip_worked_days.py
@@ -6,15 +6,20 @@ class HrPayslipWorkedDays(models.Model):
 
     number_of_days = fields.Float(readonly=False)
 
-
-
     @api.onchange('number_of_days')
-    def _onchange_pof_days(self):
+    def _onchange_number_of_days(self):
         for item in self:
-            item.amount = item.payslip_id.contract_id.wage / 30 * item.number_of_days
+            #item.amount = item.payslip_id.contract_id.wage / 30 * item.number_of_days
             
             days_not_working = item.payslip_id.contract_id.resource_calendar_id.full_days_on_month - item.number_of_days
 
             item.number_of_hours = (item.payslip_id.contract_id.resource_calendar_id.effective_days_on_month - days_not_working) * item.payslip_id.contract_id.resource_calendar_id.hours_per_day
+
+    @api.depends('number_of_days')
+    def _compute_amount(self):
+        for item in self:
+            item.amount = item.payslip_id.contract_id.wage / 30 * item.number_of_days
+            super(HrPayslipWorkedDays)._compute_amount()
+
 
     
