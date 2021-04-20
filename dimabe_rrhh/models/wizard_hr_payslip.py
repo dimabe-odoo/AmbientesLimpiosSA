048b68028d514863e1f415a037971268e44f1d70
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-20 09:00:45 -0400

    test

--- dimabe_rrhh/models/wizard_hr_payslip.py
+++ dimabe_rrhh/models/wizard_hr_payslip.py
@@ -28,7 +28,7 @@ class WizardHrPayslip(models.TransientModel):
     date_to = fields.Date('Fecha Final', required=True, default=lambda self: str(
         datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10])
 
-    def print_report_xlsx(self):
+    def print_report(self):
         raise models.ValidationError(f'in print xls book remuneration {indicator_id.name} {company_id.name}')
 
     def print_report_xlsx2(self):
