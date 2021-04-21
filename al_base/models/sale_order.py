a377b8505ff43f0c9ce7358bea3317141f4d468c
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-21 17:35:00 -0400

    defaut document_Type_id

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -3,4 +3,6 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento")
+    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento",
+                                                  default=lambda self: self.env['l10n_latam.document.type'].search(
+                                                      [('code', '=', 33)]))
