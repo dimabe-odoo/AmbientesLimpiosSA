adc658759d5c5e9c946789ee89dc6f0b4d281a94
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-21 17:38:29 -0400

    required document_Type_id

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -5,4 +5,4 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento",
                                                   default=lambda self: self.env['l10n_latam.document.type'].search(
-                                                      [('code', '=', 33)]))
+                                                      [('code', '=', 33)]),required=True)
