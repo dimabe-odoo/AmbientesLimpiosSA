dca730bdaa216337b10c406750e7afad93b75077
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-26 17:28:00 -0400

    add new group

--- al_base/__manifest__.py
+++ al_base/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/groups.xml',
         'views/product_product.xml',
         'views/product_template.xml',
         'views/res_partner.xml',
