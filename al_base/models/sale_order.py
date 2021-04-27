c0d1d8ff713ab32b81b4021c0a84b5ba77e129d2
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-27 08:39:40 -0400

    test

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -3,6 +3,14 @@ from odoo import models, fields
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    state = fields.Selection(selection_add=[('toconfirm', 'Por Confirmar')])
+
     l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', string="Tipo de Documento",
                                                   default=lambda self: self.env['l10n_latam.document.type'].search(
                                                       [('code', '=', 33)]),required=True)
+
+    
+    def order_to_confirm(self):
+        if self.state == 'draft':
+            self.state = 'toconfirm'
