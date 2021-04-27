c0fc4dd928190c787fb0eedf474756355ba11365
dimabe-odoo <desarrolloti@dimabe.cl>
2021-04-27 18:31:30 -0400

    add request and aprove date

--- al_base/models/sale_order.py
+++ al_base/models/sale_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -9,12 +10,22 @@ class SaleOrder(models.Model):
                                                   default=lambda self: self.env['l10n_latam.document.type'].search(
                                                       [('code', '=', 33)]),required=True)
 
+    request_date = fields.Datetime('Fecha de solicitud')
+
+    approve_date = fields.Datetime('Fecha de aprovación')
+
     
     def order_to_confirm(self):
         if self.state == 'draft':
             self.state = 'toconfirm'
+            self.request_date = datetime.today()
             template_id = self.env.ref('al_base.so_to_approve_mail_template')
             self.message_post_with_template(template_id.id)
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        self.approve_date = datetime.today()
+        return res
 
     @api.model
     def get_email_to(self, ref_id):
