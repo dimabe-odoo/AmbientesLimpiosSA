db1c9fa53ac6b0dbec09285223ea1726338a032e
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-04-29 16:06:53 -0400

    add product an client from odoo

--- dimabe_comercionet/models/sale_order_comercionet.py
+++ dimabe_comercionet/models/sale_order_comercionet.py
@@ -7,6 +7,7 @@ class SaleOrderComercionet(models.Model):
     _rec_name = 'purchase_order'
     purchase_order = fields.Char('Orden de compra')
     client_code_comercionet = fields.Char('Casilla Comercionet')
+    client_id = fields.Many2one('res.partner', string='Cliente')
     sale_order_id = fields.Many2one('sale.order', string='Nota de Venta')
     comercionet_line_id = fields.One2many('sale.order.comercionet.line', 'comercionet_id')
     total_price = fields.Float('Total', compute='_compute_total')
@@ -23,13 +24,16 @@ class SaleOrderComercionet(models.Model):
             for order in orders:
                 sale = self.env['sale.order.comercionet'].search([('purchase_order', '=', order['purchase_order'])])
                 if not sale:
+                    client = self.env['res.partner'].search(['comercionet_box', '=', order['client_code_comercionet']], limit=1)
                     comercionet = self.env['sale.order.comercionet'].create({
                         'purchase_order': order['purchase_order'],
                         'client_code_comercionet': order['client_code_comercionet'],
                         'doc_id': order['doc_id'],
-                        'doc': order['doc']
+                        'doc': order['doc'],
+                        'client_id': client.id if client else None
                     })
                     for line in order['lines']:
+                        product = self.env['product.product'].search(['al_dun', '=', line['product_code']], limit=1)
                         self.env['sale.order.comercionet.line'].create({
                             'number': line['number'],
                             'product_code': line['product_code'],
@@ -37,7 +41,8 @@ class SaleOrderComercionet(models.Model):
                             'price': line['price'],
                             'quantity': line['quantity'],
                             'discount_percent': line['discount_percent'],
-                            'comercionet_id': comercionet.id
+                            'comercionet_id': comercionet.id,
+                            'product_id': product.id if product else None
                         })
 
 
@@ -50,3 +55,4 @@ class SaleOrderComercionetLine(models.Model):
     quantity = fields.Integer('Cantidad')
     discount_percent = fields.Float('Descuento')
     comercionet_id = fields.Many2one('sale.order.comercionet')
+    product_id = fields.Many2one('product.product')
