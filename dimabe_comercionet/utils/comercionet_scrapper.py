6e2a3c2308e462f1a442aeba1fffde2493bcd100
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-04-29 13:26:32 -0400

    Get all pages documents

--- dimabe_comercionet/utils/comercionet_scrapper.py
+++ dimabe_comercionet/utils/comercionet_scrapper.py
@@ -33,33 +33,36 @@ def get_sale_orders():
     }
     s.post('https://www.comercionet.cl/usuarios/login.php', data=login_data)
 
-    res = s.get('https://www.comercionet.cl/listadoDocumentos.php?tido_id=3&tipo=recibidos&entrada=1')
-
-    if res.status_code == 200:
-        soup = BeautifulSoup(res.content, 'html.parser')
-
-        documents = []
-
-        # buscar los documentos dentro de la tabla y guardarlos en una lista
-        table_content = soup.find('table', {'class', 'tabla'}).find_all('tr')
-        for tr in table_content:
-            td_counter = 0
-            doc = {}
-            for td in tr.find_all('td'):
-                td_counter += 1
-                if td_counter == 1:
-                    try:
-                        doc_id = td.find('input', {'class': 'radio'}).get('value')
-                        doc['id'] = doc_id
-                    except:
-                        pass
-                if td_counter == 7 and td.text:
-                    doc['status'] = td.text
-            if doc:
-                documents.append(doc)
-
+    documents = []
+    url = 'https://www.comercionet.cl/listadoDocumentos.php?tido_id=3&tipo=recibidos&entrada=1&offset='
+    page = 0;
+    get_row = True
+    while get_row:
+        res = s.get(url+str(page))
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.content, 'html.parser')
+            # buscar los documentos dentro de la tabla y guardarlos en una lista
+            table_content = soup.find('table', {'class', 'tabla'}).find_all('tr')
+            if table_content and len(table_content) > 0:
+                for tr in table_content:
+                    td_counter = 0
+                    doc = {}
+                    for td in tr.find_all('td'):
+                        td_counter += 1
+                        if td_counter == 1:
+                            try:
+                                doc_id = td.find('input', {'class': 'radio'}).get('value')
+                                doc['id'] = doc_id
+                            except:
+                                pass
+                        if td_counter == 7 and td.text:
+                            doc['status'] = td.text
+                    if doc:
+                        documents.append(doc)
+            else:
+                get_row = False
+        page += 10
     if documents:
         sale_orders = download_documents(documents, s)
 
     return sale_orders
-
