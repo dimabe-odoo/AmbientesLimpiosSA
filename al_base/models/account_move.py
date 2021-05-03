395d5702dc9ef4189f38201a4d6759bf885613ba
dimabe-odoo <desarrolloti@dimabe.cl>
2021-05-03 10:51:11 -0400

    first way to get ted

--- al_base/models/account_move.py
+++ al_base/models/account_move.py
@@ -51,23 +51,25 @@ class AccountMove(models.Model):
         doc_id = self.env['ir.attachment'].search(
             [('res_model', '=', 'account.move'), ('res_id', '=', self.id), ('name', 'like', 'SII')]).datas
         doc_xml = base64.b64encode(doc_id).decode('ascii')
-        # with open(doc_xml) as xml_file:
-        data_dict = xmltodict.parse(doc_xml)
-        json_data = json.dumps(data_dict['EnvioDTE']['SetDTE']['DTE']['Documento']['TED'])
-        cols = 12
-        while True:
-            try:
-                if cols == 31:
+        xml_result = open('doc.xml','wb')
+        xml_result.write(doc_xml)
+        with open(xml_result) as xml_file:
+            data_dict = xmltodict.parse(xml_file.read())
+            json_data = json.dumps(data_dict['EnvioDTE']['SetDTE']['DTE']['Documento']['TED'])
+            cols = 12
+            while True:
+                try:
+                    if cols == 31:
+                        break
+                    codes = encode(json_data, cols)
+                    image = render_image(codes, scale=5, ratio=2)
+                    buffered = BytesIO()
+                    image.save(buffered, format="JPEG")
+                    img_str = base64.b64encode(buffered.getvalue())
+                    self.write({'ted': img_str})
                     break
-                codes = encode(json_data, cols)
-                image = render_image(codes, scale=5, ratio=2)
-                buffered = BytesIO()
-                image.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue())
-                self.write({'ted': img_str})
-                break
-            except:
-                cols += 1
+                except:
+                    cols += 1
 
 
 @api.model
