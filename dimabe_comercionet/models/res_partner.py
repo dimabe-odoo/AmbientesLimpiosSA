6f3dfc185450dd0c79c7bd3fb141fabb030f0799
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-05-05 09:48:41 -0400

    remove write and create method of partnert

--- dimabe_comercionet/models/res_partner.py
+++ dimabe_comercionet/models/res_partner.py
@@ -3,15 +3,3 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     comercionet_box = fields.Char('Casilla Comercionet')
-
-    @api.model
-    def create(self,values):
-        if 'comercionet_box' in values.keys():
-            values['comercionet_box'] = values['comercionet_box'].strip()
-        return super(ResPartner, self).create(values)
-
-    @api.model
-    def write(self, values):
-        if 'comercionet_box' in values.keys():
-            values['comercionet_box'] = values['comercionet_box'].strip()
-        return super(ResPartner, self).write(values)
