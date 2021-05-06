91605b2aa46febe3c6321c11fe23174a8b1f2e45
dimabe-odoo <desarrolloti@dimabe.cl>
2021-05-06 11:30:16 -0400

    remove raise

--- dimabe_comercionet/utils/download_pdf.py
+++ dimabe_comercionet/utils/download_pdf.py
@@ -3,6 +3,7 @@ import pdfkit
 from odoo.tools.misc import find_in_path
 from odoo import models
 from bs4 import BeautifulSoup
+from odoo.http import request
 from urllib.parse import urljoin
 import base64
 import urllib.request
@@ -18,9 +19,6 @@ def download_pdfs(documents):
     result = []
     for doc in documents:
         url = "https://www.comercionet.cl/visualizacion/visualizar_documentoORDERS.php?tipo=recibidos&docu_id="+doc
-        response = urllib.request.urlopen(url)
-        webContent = response.read()
-        raise models.ValidationError(webContent)
         cookies = []
         for key, value in s.cookies.get_dict().items():
             cookies.append((key, value))
@@ -28,8 +26,11 @@ def download_pdfs(documents):
         
         options = {'cookie': cookies}
         # verificar configuración de wkhtmltopdf en odoo sh
-        pdf = pdfkit.from_string(web.text,"order.pdf", options=options)
-        with open("order.pdf",'rb') as
+        path = request.env['ir.config_parameter'].search([('key', '=', 'webkit')])
+        config = pdfkit.configuration(wkhtmltopdf=path.value)
+        pdfkit.from_url(url, "order.pdf", options=options, configuration=config)
+        with open("order.pdf", "rb") as pdf_file:
+            pdf_b64 = base64.b64encode(pdf_file.read())
         result.append({'doc_id': doc, 'pdf_file': pdf_b64})
     return result
 
