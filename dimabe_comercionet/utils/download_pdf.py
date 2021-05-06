4aa67c05bc9ceeff240406501f6c3f4677bf3519
Leonardo Norambuena <leonardonorambuena@MacBook-Air-de-Leonardo.local>
2021-05-06 17:35:45 -0400

    remove configuration pdfkit

--- dimabe_comercionet/utils/download_pdf.py
+++ dimabe_comercionet/utils/download_pdf.py
@@ -11,7 +11,6 @@ import codecs
 
 def download_pdfs(documents):
     s = requests.session()
-    s.verify = False
     login_data = {
             'login': '7808800014004',
             'password': 'Alimpsaexcell2021'
@@ -22,12 +21,13 @@ def download_pdfs(documents):
         url = "https://www.comercionet.cl/visualizacion/visualizar_documentoORDERS.php?tipo=recibidos&docu_id="+doc
         cookies = []
         for key, value in s.cookies.get_dict().items():
-            cookies.append((key, value))   
+            cookies.append((key, value))
+
+        
         options = {'cookie': cookies}
         # verificar configuración de wkhtmltopdf en odoo sh
         config = pdfkit.configuration(wkhtmltopdf=bytes(find_in_path('wkhtmltopdf'),'utf-8'))
-        pdf = pdfkit.from_url(url,False, options=options, configuration=config)
-        models.ValidationError(pdf)
+        pdfkit.from_url(url, "order.pdf", options=options)
         with open("order.pdf", "rb") as pdf_file:
             pdf_b64 = base64.b64encode(pdf_file.read())
         result.append({'doc_id': doc, 'pdf_file': pdf_b64})
