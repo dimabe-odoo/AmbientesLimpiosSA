706399aefa844e04fd633f15df78a9e3f5515a6a
dimabe-odoo <desarrolloti@dimabe.cl>
2021-05-10 17:22:13 -0400

    add more validations in create-write range

--- al_base/models/custom_range_approve_purchase.py
+++ al_base/models/custom_range_approve_purchase.py
@@ -1,6 +1,7 @@
 from odoo import fields, models, api
 from ..utils.valid_range_approve import valid_range
 
+
 class CustomRangeApprovePurchase(models.Model):
     _name = 'custom.range.approve.purchase'
 
@@ -11,26 +12,33 @@ class CustomRangeApprovePurchase(models.Model):
 
     @api.model
     def create(self, values):
+        res = super(CustomRangeApprovePurchase, self).create(values)
         if values['max_amount'] > values['min_amount'] or values['max_amount'] == 0:
-            if self.valid_range(values):
-                return super(CustomRangeApprovePurchase, self).create(values)
+            if self.valid_range_create(values, res.id):
+                return res
         else:
-            raise models.ValidationError('Monto máximo debe ser mayor que el monto mínimo, excepto si el monto máximo no tiene límite, éste debe ser 0')
+            raise models.ValidationError(
+                'Monto máximo debe ser mayor que el monto mínimo, excepto si el monto máximo no tiene límite, éste debe ser 0')
 
     def write(self, values):
-        if 'max_amount' in values.keys() and 'min_amount' not in values.keys():
+        res = super(CustomRangeApprovePurchase, self).write(values)
+        if 'min_amount' not in values.keys():
             values['min_amount'] = self.min_amount
-        if 'max_amount' not in values.keys() and 'min_amount' in values.keys():
+        if 'max_amount' not in values.keys():
             values['max_amount'] = self.max_amount
         if 'max_amount' in values.keys() and 'min_amount' in values.keys():
             if values['max_amount'] > values['min_amount'] or values['max_amount'] == 0:
-                if self.valid_range(values):
-                    return super(CustomRangeApprovePurchase, self).write(values)
+                if self.valid_range_write(values):
+                    return res
             else:
-                raise models.ValidationError('Monto máximo debe ser mayor que el monto mínimo, excepto si el monto máximo no tiene límite, éste debe ser 0')
-
-    def valid_range(self, values):
-        range_ids = self.env['custom.range.approve.purchase'].search([])
-        valid_range(values, range_ids)
+                raise models.ValidationError(
+                    'Monto máximo debe ser mayor que el monto mínimo, excepto si el monto máximo no tiene límite, éste debe ser 0')
 
 
+    def valid_range_create(self, values, id):
+        range_ids = self.env['custom.range.approve.purchase'].search([('id', '!=', id)])
+        return valid_range(values, range_ids)
+
+    def valid_range_write(self, values):
+        range_ids = self.env['custom.range.approve.purchase'].search([('id', '!=', self.id)])
+        return valid_range(values, range_ids)
