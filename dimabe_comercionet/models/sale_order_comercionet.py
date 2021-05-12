e61a909821574af29160ef1d5b5b4efdc4070fcc
Leonardo Norambuena <leonardo.norambuenam@gmail.com>
2021-05-12 10:58:58 -0400

    search product by ean

--- dimabe_comercionet/models/sale_order_comercionet.py
+++ dimabe_comercionet/models/sale_order_comercionet.py
@@ -69,10 +69,27 @@ class SaleOrderComercionet(models.Model):
                 })
         return res
 
+    def get_product(self, product_code):
+        product = self.env['product.product'].search([('al_dun', '=', product_code)], limit=1)
+        if not product:
+            p_tmpl = self.env['product.template'].search([('al_dun', '=', product_code)], limit=1)
+            if p_tmpl:
+                product = self.env['product.product'].search([('product_tmpl_id', '=', p_tmpl.id)])
+                if product:
+                    product.write({
+                        'al_dun': p_tmpl.al_dun,
+                        'al_sku': p_tmpl.al_sku
+                    })
+            else:
+                product = self.env['product.product'].search([('barcode', '=', product_code)], limit=1)
+                if not product:
+                    product = self.env['product.template'].search([('barcode', '=', product_code)], limit=1)
+        return product
+
     def update_product_id_line(self):
         for line in self.comercionet_line_id:
             if not line.product_id:
-                product = self.env['product.product'].search([('al_dun','=',line.product_code)])
+                product = self.get_product(line.product_code)
                 line.write({
                     'product_id': product.id
                 })
@@ -190,16 +207,7 @@ class SaleOrderComercionet(models.Model):
                     })
                     for line in order['lines']:
                         product_code = line['product_code'].strip()
-                        product = self.env['product.product'].search([('al_dun', '=', product_code)], limit=1)
-                        if not product:
-                            p_tmpl = self.env['product.template'].search([('al_dun', '=', product_code)], limit=1)
-                            if p_tmpl:
-                                product = self.env['product.product'].search([('product_tmpl_id', '=', p_tmpl.id)])
-                                if product:
-                                    product.write({
-                                        'al_dun': p_tmpl.al_dun,
-                                        'al_sku': p_tmpl.al_sku
-                                    })
+                        product = self.get_product(product_code)
                         self.env['sale.order.comercionet.line'].create({
                             'number': line['number'],
                             'product_code': product_code,
