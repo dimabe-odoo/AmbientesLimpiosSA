6b5d12e10739d79be380af5d323a90da7f3a9bf2
dimabe-odoo <desarrolloti@dimabe.cl>
2021-06-16 13:07:46 -0400

    fix error in res_partner

--- al_base/models/res_partner.py
+++ al_base/models/res_partner.py
@@ -5,6 +5,7 @@ from ..utils.rut_helper import RutHelper
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.model
     def create(self,values):
         if isinstance(values,list):
             for val in values:
@@ -13,11 +14,12 @@ class ResPartner(models.Model):
                     if parent_id.l10n_cl_dte_email and parent_id.l10n_cl_dte_email != '':
                         val['l10n_cl_dte_email'] = parent_id.l10n_cl_dte_email
                         val['l10n_cl_activity_description'] = parent_id.l10n_cl_activity_description
-            return super(ResPartner, self).create(values)
         else:
             if 'parent_id' in values.keys():
                 parent_id = self.env['res.partner'].search([('id', '=', values['parent_id'])])
                 if parent_id.l10n_cl_dte_email and parent_id.l10n_cl_dte_email != '':
                     values['l10n_cl_dte_email'] = parent_id.l10n_cl_dte_email
                     values['l10n_cl_activity_description'] = parent_id.l10n_cl_activity_description
+
+        return super(ResPartner, self).create(values)
 
