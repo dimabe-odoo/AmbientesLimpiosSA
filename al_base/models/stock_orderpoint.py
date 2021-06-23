da8a3c3c11f383399c8751193989c48aca93efb2
dimabe-odoo <desarrolloti@dimabe.cl>
2021-06-23 09:55:11 -0400

    fix

--- al_base/models/stock_orderpoint.py
+++ al_base/models/stock_orderpoint.py
@@ -5,8 +5,13 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.model
     def create(self, values):
-        for val in values:
-            if 'location_id' in val.keys():
-                location = self.env['stock.location'].sudo().search([('id', '=', val['location_id'])])
-                val['warehouse_id'] = location.get_warehouse().id
+        if isinstance(values,list):
+            for val in values:
+                if 'location_id' in val.keys():
+                    location = self.env['stock.location'].sudo().search([('id', '=', val['location_id'])])
+                    val['warehouse_id'] = location.get_warehouse().id
+        else:
+            if 'location_id' in values.keys():
+                location = self.env['stock.location'].sudo().search([('id', '=', values['location_id'])])
+                values['warehouse_id'] = location.get_warehouse().id
         return super(StockWarehouseOrderpoint, self).create(values)
